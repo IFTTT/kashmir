985c9d0087f633025ca2a55c9810f8bacf1bf5ae
Netto Farah <nettofarah@gmail.com>
2015-04-09 13:00:40 -0700

    represent methods

--- lib/kashmir.rb
+++ lib/kashmir.rb
@@ -7,11 +7,13 @@ module Kashmir
   end
 
 
-  def represent(field_groups=[])
+  def represent(representation_titles=[])
     representation = {}
+    representation_titles << :base
 
-    self.class.definitions[:base].each do |field|
-      representation[field] = instance_variable_get("@#{field}")
+    representation_titles.each do |representation_title|
+      represented_definition = self.class.definitions[representation_title].run_for(self)
+      representation = representation.merge(represented_definition)
     end
 
     representation
@@ -24,12 +26,42 @@ module Kashmir
     end
 
     def base(fields=[])
-      @@_definitions ||= {}
-      @@_definitions[:base] = fields
+      rep(:base, fields)
+    end
+
+    def rep(title, fields)
+      definitions[title] = Representation.new(title, fields)
     end
 
     def definitions
+      @@_definitions ||= {}
       @@_definitions
+    end
+  end
+
+  class Representation
+
+    def initialize(title, fields)
+      @title = title
+      @fields = fields
+    end
+
+    def run_for(instance)
+      representation = {}
+
+      instance_vars = instance.instance_variables
+
+      @fields.each do |field|
+        value = if instance.respond_to?(field)
+                  instance.send(field)
+                else
+                  instance.instance_variable_get("@#{field}")
+                end
+
+        representation[field] = value
+      end
+
+      representation
     end
   end
 end
--- test/kashmir_test.rb
+++ test/kashmir_test.rb
@@ -6,16 +6,33 @@ describe Kashmir do
     include Kashmir
 
     representations do
-      base([:title])
+      base([:title, :preparation_time])
+      rep(:num_steps, [:num_steps])
     end
 
-    def initialize(title)
+    def initialize(title, preparation_time, steps=[])
       @title = title 
+      @preparation_time = preparation_time
+      @steps = steps
+    end
+
+    def num_steps
+      @steps.size
     end
   end
 
   it 'renders basic attribute representations' do
-    recipe = Recipe.new('Beef stew')
-    assert_equal recipe.represent, { title: 'Beef stew' }
+    recipe = Recipe.new('Beef stew', 60)
+    assert_equal recipe.represent, { title: 'Beef stew', preparation_time: 60 }
+  end
+
+  it 'renders basic calculated representations' do
+    recipe = Recipe.new('Beef stew', 60, ['chop', 'cook'])
+
+    assert_equal recipe.represent([:num_steps]), { 
+      title: 'Beef stew', 
+      preparation_time: 60, 
+      num_steps: 2 
+    }
   end
 end
