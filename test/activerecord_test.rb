d3187ae1d7f0c9953c58ac968a8bddb49856ae0b
Netto Farah <nettofarah@gmail.com>
2015-04-09 20:38:30 -0700

    basic belongs_to testcases

--- test/activerecord_test.rb
+++ test/activerecord_test.rb
@@ -5,13 +5,51 @@ require 'ar_test_helper'
 describe 'ActiveRecord integration' do
 
   before(:each) do
-    @pastrami_sandwich = AR::Recipe.create(title: 'Pastrami Sandwich')
+    @tom = AR::Chef.create(name: 'Tom')
+    @pastrami_sandwich = AR::Recipe.create(title: 'Pastrami Sandwich', chef: @tom)
+    @restaurant = AR::Restaurant.create(name: 'Chef Tom Belly Burgers', owner: @tom)
   end
 
   it 'represents ar objects' do
     ps = @pastrami_sandwich.represent_with do
       prop :title
     end
+
     assert_equal ps, { title: 'Pastrami Sandwich' }
+  end
+
+  describe 'belongs_to' do
+
+    it 'works for basic relations' do
+      ps = @pastrami_sandwich.represent_with do
+        prop :title
+        inline :chef do
+          prop :name
+        end
+      end
+
+      assert_equal ps, {
+        title: 'Pastrami Sandwich',
+        chef: {
+          name: 'Tom'
+        }
+      }
+    end
+
+    it 'works with custom names' do
+      r = @restaurant.represent_with do
+        prop :name
+        inline :owner do
+          prop :name
+        end
+      end
+
+      assert_equal r, {
+        name: 'Chef Tom Belly Burgers',
+        owner: {
+          name: 'Tom'
+        }
+      }
+    end
   end
 end
--- test/support/ar_models.rb
+++ test/support/ar_models.rb
@@ -2,8 +2,11 @@ module AR
   class Recipe < ActiveRecord::Base
     include Kashmir
 
+    belongs_to :chef
+
     representations do
       rep :title
+      rep :chef
     end
   end
 
@@ -13,6 +16,22 @@ module AR
   class RecipeIngredient
   end
 
+  class Restaurant < ActiveRecord::Base
+    include Kashmir
+
+    belongs_to :owner, class_name: 'Chef'
+
+    representations do
+      rep :name
+      rep :owner
+    end
+  end
+
   class Chef < ActiveRecord::Base
+    include Kashmir
+
+    representations do
+      rep :name
+    end
   end
 end
