6edb268c06ec03a89a345bb1e1e4d3c652f09ef7
Netto Farah <nettofarah@gmail.com>
2015-07-22 13:07:39 -0700

    remove dependency to colorize

--- lib/kashmir/caching.rb
+++ lib/kashmir/caching.rb
@@ -1,19 +1,18 @@
 require 'kashmir/plugins/memory_caching'
 require 'kashmir/plugins/null_caching'
-require 'colorize'
 
 module Kashmir
   module Caching
 
     def from_cache(representation_definition, object)
-      log("#{"read".blue}: #{log_key(object, representation_definition)}", :debug)
+      log("read: #{log_key(object, representation_definition)}", :debug)
 
       cached_representation = Kashmir.caching.from_cache(representation_definition, object)
 
       if cached_representation
-        log("#{"hit".green}: #{log_key(object, representation_definition)}")
+        log("hit: #{log_key(object, representation_definition)}")
       else
-        log("#{"miss".red}: #{log_key(object, representation_definition)}")
+        log("miss: #{log_key(object, representation_definition)}")
       end
 
       cached_representation
@@ -21,18 +20,18 @@ module Kashmir
 
     def bulk_from_cache(representation_definition, objects)
       class_name = objects.length > 0 ? objects.first.class.to_s : ''
-      log("#{"read_multi".blue}: [#{objects.length}]#{class_name} : #{representation_definition}", :debug)
+      log("read_multi: [#{objects.length}]#{class_name} : #{representation_definition}", :debug)
       Kashmir.caching.bulk_from_cache(representation_definition, objects)
     end
 
     def store_presenter(representation_definition, representation, object, ttl)
-      log("#{"write".blue} TTL: #{ttl}: #{log_key(object, representation_definition)}", :debug)
+      log("write TTL: #{ttl}: #{log_key(object, representation_definition)}", :debug)
       Kashmir.caching.store_presenter(representation_definition, representation, object, ttl)
     end
 
     def bulk_write(representation_definition, representations, objects, ttl)
       class_name = objects.length > 0 ? objects.first.class.to_s : ''
-      log("#{"write_multi".blue}: TTL: #{ttl}: [#{objects.length}]#{class_name} : #{representation_definition}", :debug)
+      log("write_multi: TTL: #{ttl}: [#{objects.length}]#{class_name} : #{representation_definition}", :debug)
       Kashmir.caching.bulk_write(representation_definition, representations, objects, ttl)
     end
 
@@ -41,7 +40,7 @@ module Kashmir
     end
 
     def log(message, level=:info)
-      Kashmir.logger.send(level, ("\n#{"Kashmir::Caching".magenta} #{message}\n"))
+      Kashmir.logger.send(level, ("\nKashmir::Caching #{message}\n"))
     end
 
     module_function :from_cache, :bulk_from_cache, :bulk_write, :store_presenter, :log_key, :log
